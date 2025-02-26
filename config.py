21d2006bd5d42cde3038507147a563c82e39f1a1
haiderareeb <haiderpb03@gmail.com>
2025-02-26 08:56:10 +0530

    deleting files

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'hello123'
-    # Update with the credentials we just created
-    SQLALCHEMY_DATABASE_URI = 'mysql://ecommerce_user:password123@localhost/ecommerce_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join('app', 'static', 'product_images')
